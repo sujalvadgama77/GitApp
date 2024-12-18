dfc9a68c0af50b1ea3cfeead34d624c80b68fb1c
Sujal Vadgama <mrvadgamas@gmail.com>
2024-12-18 18:30:31 +0530

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-const http = require('http'); // Import the HTTP module
-
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-    res.statusCode = 200; // HTTP status for success
-    res.setHeader('Content-Type', 'text/plain'); // Content type
-    res.end('Jai Shree Krishna!\n'); // Response body
-});
-
-const PORT = 3000; // Define a port number
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Welcom to Node Appllication Finally !!!'));
+app.listen(port, () => console.log('Server is Runninf on port' + port));
